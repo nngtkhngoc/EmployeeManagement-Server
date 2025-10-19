c0e2d8ff8a2f7e113632cb247dcdc080bcd977d3
Ranizt <chicuongvo3@gmail.com>
2025-10-19 14:13:34 +0700

    employee module

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,10 +1,10 @@
-import { employeeService } from "../services/employee.service.js";
 import employeeValidation from "../validations/employee.validation.js";
+import employeeService from "../services/employee.service.js";
 
 const employeeController = {
   getAllEmployees: async (req, res) => {
     try {
-      const employees = await employeeService.getAllEmployees();
+      const employees = await employeeService.read();
 
       return res.status(200).json({ data: employees });
     } catch (error) {
@@ -21,7 +21,6 @@ const employeeController = {
         .validateAsync(req.body, {
           abortEarly: false,
         });
-
       const employeeData = {
         fullName: value.fullName,
         avatar: value.avatar,
@@ -39,26 +38,21 @@ const employeeController = {
         departmentId: value.departmentId,
         positionId: value.positionId,
       };
-
       const newEmployee = await employeeService.createEmployee(employeeData);
-
       return res.status(201).json({ data: newEmployee });
     } catch (error) {
       console.log("Error create new employee ", error);
-
       if (
         error.message.includes("Department không tồn tại") ||
         error.message.includes("Position không tồn tại")
       ) {
         return res.status(400).json({ message: error.message });
       }
-
       if (error.isJoi) {
         return res.status(400).json({
-          errors: error.details.map((d) => d.message),
+          errors: error.details.map(d => d.message),
         });
       }
-
       return res.status(500).send();
     }
   },
--- services/employee.service.js
+++ services/employee.service.js
@@ -1,49 +1,11 @@
+import BaseService from "../common/service/baseService.js";
 import { hashPassword } from "../config/bcrypt.js";
 import { prisma } from "../config/db.js";
 
-export const employeeService = {
-  getAllEmployees: async () => {
-    return prisma.employee.findMany();
-  },
+class EmployeeService extends BaseService {
+  constructor(repository) {
+    super(repository);
+  }
+}
 
-  createEmployee: async (employeeData) => {
-    return prisma.$transaction(async (tx) => {
-      const department = await tx.department.findUnique({
-        where: { id: employeeData.departmentId },
-      });
-      if (!department) throw new Error("Department không tồn tại");
-
-      const position = await tx.position.findUnique({
-        where: { id: employeeData.positionId },
-      });
-      if (!position) throw new Error("Position không tồn tại");
-
-      const nextVal =
-        await tx.$queryRaw`SELECT nextval('employee_code_seq') as seq;`;
-      const seqNum = nextVal[0].seq;
-      const employeeCode = "EM" + seqNum.toString().padStart(3, "0");
-
-      const password = employeeCode + department.departmentCode;
-      const hashedPassword = await hashPassword(password);
-
-      const { departmentId, positionId } = employeeData;
-      delete employeeData.departmentId;
-      delete employeeData.positionId;
-      const newEmployee = await tx.employee.create({
-        data: {
-          ...employeeData,
-          position: { connect: { id: positionId } },
-          department: { connect: { id: departmentId } },
-          employeeCode,
-          password: hashedPassword,
-        },
-        include: {
-          position: true,
-          department: true,
-        },
-      });
-
-      return newEmployee;
-    });
-  },
-};
+export default new EmployeeService(prisma.employee);
