8e5f283ec3c0d18d81680c3e3f45f3f3576f778d
Ranizt <chicuongvo3@gmail.com>
2025-10-19 18:46:34 +0700

    leave-request module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import employeeRouter from "../modules/employee/employee.route.js";
+import leaveApplicationRouter from "../modules/leave-application/leave-application.route.js";
 
 export const routes = app => {
   app.use("/api/employee", employeeRouter);
   app.use("/healthcheck", (req, res) => res.status(200).send("OK"));
+  app.use("/api/leave-application", leaveApplicationRouter);
 };
