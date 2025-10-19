12d6f52351443e790fa8c776bfce4f1a4faf8e42
Ranizt <chicuongvo3@gmail.com>
2025-10-19 14:13:06 +0700

    core base service

---
+++ common/service/baseService.js
@@ -0,0 +1,67 @@
+export const DELETE_OPTION = {
+  SOFT: "soft",
+  HARD: "hard",
+};
+
+export default class BaseService {
+  repository;
+  constructor(repository) {
+    this.repository = repository;
+  }
+
+  async create(data) {
+    return this.repository.create({ data });
+  }
+
+  async createMany(data) {
+    return this.repository.create({ data, skipDuplicates: true });
+  }
+
+  async read(filter) {
+    return this.repository.findMany(filter);
+  }
+
+  async readOne(filter) {
+    return this.repository.findFirst(filter);
+  }
+
+  async readById(id) {
+    return this.repository.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(filter, data) {
+    return this.repository.update({
+      where: filter,
+      data,
+    });
+  }
+
+  async updateMany(filter) {
+    return this.repository.update({
+      where: filter,
+      data,
+    });
+  }
+
+  async delete(filter, option = DELETE_OPTION.HARD) {
+    if (option === DELETE_OPTION.SOFT)
+      return this.repository.update({
+        where: filter,
+        data: { isActive: false },
+      });
+
+    return this.repository.delete(filter);
+  }
+
+  async deleteMany(filter, option = DELETE_OPTION.HARD) {
+    if (option === DELETE_OPTION.SOFT)
+      return this.repository.updateMany({
+        where: filter,
+        data: { isActive: false },
+      });
+
+    return this.repository.delete(filter);
+  }
+}
